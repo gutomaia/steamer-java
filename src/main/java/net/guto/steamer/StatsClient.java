cf18014010ad42a2f4d8994be575711f024776e3
Guto Maia <guto@guto.net>
2011-12-06 16:47:27 -0200

    refactoring, Stats gameLogo property is being parsed

--- src/main/java/net/guto/steamer/StatsClient.java
+++ src/main/java/net/guto/steamer/StatsClient.java
@@ -14,7 +14,8 @@ public class StatsClient {
 		GAME_FRIENDLY_NAME("/playerstats/game/gameFriendlyName", STRING),
 		GAME_NAME("/playerstats/game/gameName", STRING),
 		GAME_LINK("/playerstats/game/gameLink", STRING),
-		GAME_ICON("/playerstats/game/gameIcon", STRING);
+		GAME_ICON("/playerstats/game/gameIcon", STRING),
+		GAME_LOGO("/playerstats/game/gameLogo", STRING);
 
 		private final String xpath;
 		private final QName dataType;
@@ -36,13 +37,10 @@ public class StatsClient {
 	public Stats getStats(String username, String game) {
 		Stats stats = new Stats();
 		if (game.equals("portal2")) {
-			stats.gameLogo = "http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502.jpg";
 			stats.gameLogoSmall = "http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502_thumb.jpg";
 		} else if (game.equals("tf2")) {
-			stats.gameLogo = "http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8.jpg";
 			stats.gameLogoSmall = "http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8_thumb.jpg";
 		} else if (game.equals("l4d2")) {
-			stats.gameLogo = "http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg";
 			stats.gameLogoSmall = "http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142_thumb.jpg";
 		}
 		Document document = getDocument("src/test/resources/" + username + "-" + game + ".xml");
@@ -50,6 +48,7 @@ public class StatsClient {
 		stats.gameName = getStringValue(StatsField.GAME_NAME, document);
 		stats.gameLink = getStringValue(StatsField.GAME_LINK, document);
 		stats.gameIcon = getStringValue(StatsField.GAME_ICON, document);
+		stats.gameLogo = getStringValue(StatsField.GAME_LOGO, document);
 		return stats;
 	}
 }

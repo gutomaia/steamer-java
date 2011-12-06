7cc62cdad6e4ba79847143319708a9aadb1d3137
Guto Maia <guto@guto.net>
2011-12-06 00:53:54 -0200

    Now with multiple getValues types

--- src/main/java/net/guto/steamer/SteamUser.java
+++ src/main/java/net/guto/steamer/SteamUser.java
@@ -1,12 +1,14 @@
 package net.guto.steamer;
 
+import static javax.xml.xpath.XPathConstants.STRING;
+
 import java.io.IOException;
 
+import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
@@ -17,15 +19,20 @@ import org.xml.sax.SAXException;
 public class SteamUser {
 
 	private enum Field {
-		STEAM_ID("//profile/steamID"), AVATAR_ICON("//profile/avatarIcon"), AVATAR_MEDIUM("//profile/avatarMedium"), AVATAR_FULL(
-				"//profile/avatarFull"), CUSTOM_URL("//profile/customURL");
+		STEAM_ID64("//profile/steamID64", STRING),
+		STEAM_ID("//profile/steamID", STRING),
+		AVATAR_ICON("//profile/avatarIcon", STRING),
+		AVATAR_MEDIUM("//profile/avatarMedium", STRING),
+		AVATAR_FULL("//profile/avatarFull", STRING),
+		CUSTOM_URL("//profile/customURL", STRING);
 
 		public final String xpath;
+		public final QName dataType;
 
-		private Field(String xpath) {
+		private Field(String xpath, QName dataType) {
 			this.xpath = xpath;
+			this.dataType = dataType;
 		}
-
 	}
 
 	String username;
@@ -37,19 +44,28 @@ public class SteamUser {
 
 	protected void parseProfile() {
 		Document document = getDocument();
-		steamID = getValue(Field.STEAM_ID, document);
-		avatarIcon = getValue(Field.AVATAR_ICON, document);
-		avatarMedium = getValue(Field.AVATAR_MEDIUM, document);
-		avatarFull = getValue(Field.AVATAR_FULL, document);
-		customUrl = getValue(Field.CUSTOM_URL, document);
+		steamId64 = getLongValue(Field.STEAM_ID64, document);
+		steamID = getStringValue(Field.STEAM_ID, document);
+		avatarIcon = getStringValue(Field.AVATAR_ICON, document);
+		avatarMedium = getStringValue(Field.AVATAR_MEDIUM, document);
+		avatarFull = getStringValue(Field.AVATAR_FULL, document);
+		customUrl = getStringValue(Field.CUSTOM_URL, document);
+	}
+	
+	private Long getLongValue(Field field, Document document){
+		return Long.parseLong((String) getValue(field, document));
 	}
 
-	private String getValue(Field field, Document document) {
+	private String getStringValue(Field field, Document document) {
+		return (String) getValue(field, document);
+	}
+
+	private Object getValue(Field field, Document document) {
 		try {
 			XPathFactory xpathFactory = XPathFactory.newInstance();
 			XPath xpath = xpathFactory.newXPath();
 			XPathExpression expression = xpath.compile(field.xpath);
-			return (String) expression.evaluate(document, XPathConstants.STRING);
+			return expression.evaluate(document, field.dataType);
 		} catch (XPathExpressionException e) {
 			e.printStackTrace();
 			return null;
@@ -74,6 +90,7 @@ public class SteamUser {
 		}
 	}
 
+	Long steamId64;
 	String steamID;
 	String avatarIcon;
 	String avatarMedium;
@@ -81,8 +98,7 @@ public class SteamUser {
 	String customUrl;
 
 	public Long getSteamId64() {
-		if (username.equals("gobli")) return 76561198029691735l;
-		return 76561197985077150l;
+		return steamId64;
 	}
 
 	public String getSteamId() {

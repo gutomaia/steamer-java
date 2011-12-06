dec68aafb950a52ec3fb4283f32bfb0b989a49b6
Guto Maia <guto@guto.net>
2011-12-06 16:40:20 -0200

    refactoring, Stats gameIcon property is being parsed

--- src/main/java/net/guto/steamer/StatsClient.java
+++ src/main/java/net/guto/steamer/StatsClient.java
@@ -13,7 +13,8 @@ public class StatsClient {
 	private static enum StatsField implements Field {
 		GAME_FRIENDLY_NAME("/playerstats/game/gameFriendlyName", STRING),
 		GAME_NAME("/playerstats/game/gameName", STRING),
-		GAME_LINK("/playerstats/game/gameLink", STRING);
+		GAME_LINK("/playerstats/game/gameLink", STRING),
+		GAME_ICON("/playerstats/game/gameIcon", STRING);
 
 		private final String xpath;
 		private final QName dataType;
@@ -35,15 +36,12 @@ public class StatsClient {
 	public Stats getStats(String username, String game) {
 		Stats stats = new Stats();
 		if (game.equals("portal2")) {
-			stats.gameIcon = "http://media.steampowered.com/steamcommunity/public/images/apps/620/2e478fc6874d06ae5baf0d147f6f21203291aa02.jpg";
 			stats.logo = "http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502.jpg";
 			stats.logoSmall = "http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502_thumb.jpg";
 		} else if (game.equals("tf2")) {
-			stats.gameIcon = "http://media.steampowered.com/steamcommunity/public/images/apps/440/e3f595a92552da3d664ad00277fad2107345f743.jpg";
 			stats.logo = "http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8.jpg";
 			stats.logoSmall = "http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8_thumb.jpg";
 		} else if (game.equals("l4d2")) {
-			stats.gameIcon = "http://media.steampowered.com/steamcommunity/public/images/apps/550/7d5a243f9500d2f8467312822f8af2a2928777ed.jpg";
 			stats.logo = "http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg";
 			stats.logoSmall = "http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142_thumb.jpg";
 		}
@@ -51,6 +49,7 @@ public class StatsClient {
 		stats.gameFriendlyName = getStringValue(StatsField.GAME_FRIENDLY_NAME, document);
 		stats.gameName = getStringValue(StatsField.GAME_NAME, document);
 		stats.gameLink = getStringValue(StatsField.GAME_LINK, document);
+		stats.gameIcon = getStringValue(StatsField.GAME_ICON, document);
 		return stats;
 	}
 }

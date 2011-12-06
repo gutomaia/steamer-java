ca6e99a2ec2dd113f01a3f8058464b75a303291f
Guto Maia <guto@guto.net>
2011-12-06 17:41:18 -0200

    fake stub tests for achievements feature

--- src/main/java/net/guto/steamer/Achievement.java
+++ src/main/java/net/guto/steamer/Achievement.java
@@ -2,4 +2,23 @@ package net.guto.steamer;
 
 public class Achievement {
 
+	public String getIconClosed() {
+		return "http://media.steampowered.com/steamcommunity/public/images/apps/550/8d94a8ffcb20fd11de547a6972bcef319fd93bf6.jpg";
+	}
+
+	public String getIconOpen() {
+		return "http://media.steampowered.com/steamcommunity/public/images/apps/550/19c4af0f9bb24b719a086cbd3b63961d8348bf2b.jpg";
+	}
+
+	public String getName() {
+		return "ACID REFLEX";
+	}
+
+	public String getApiname() {
+		return "ach_kill_spitter_fast";
+	}
+
+	public String getDescription() {
+		return "Kill a Spitter before she is able to spit.";
+	}
 }
--- src/test/java/net/guto/steamer/StatsTest.java
+++ src/test/java/net/guto/steamer/StatsTest.java
@@ -6,10 +6,19 @@ import junit.framework.TestCase;
 
 public class StatsTest extends TestCase {
 
-	public void testL4D2Stats() {
+	public void testAchievements() {
 		StatsClient client = new StatsClient();
 		List<Achievement> achievements = client.getAchievements("gutomaia", "l4d2");
 		assertNotNull(achievements);
 		assertEquals(67, achievements.size());
+		Achievement achievement = achievements.get(0);
+
+		assertEquals("http://media.steampowered.com/steamcommunity/public/images/apps/550/8d94a8ffcb20fd11de547a6972bcef319fd93bf6.jpg",
+				achievement.getIconClosed());
+		assertEquals("http://media.steampowered.com/steamcommunity/public/images/apps/550/19c4af0f9bb24b719a086cbd3b63961d8348bf2b.jpg",
+				achievement.getIconOpen());
+		assertEquals("ACID REFLEX", achievement.getName());
+		assertEquals("ach_kill_spitter_fast", achievement.getApiname());
+		assertEquals("Kill a Spitter before she is able to spit.", achievement.getDescription());
 	}
 }
